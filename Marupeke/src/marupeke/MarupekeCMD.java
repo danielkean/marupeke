96051c59594761f9430699a4852eb5db54bbc765
Daniel Kean <danielkean99@hotmail.com>
2020-07-04 13:23:22 +0100

    Update command line interface

--- Marupeke/src/marupeke/MarupekeCMD.java
+++ Marupeke/src/marupeke/MarupekeCMD.java
@@ -4,56 +4,78 @@ import java.util.Scanner;
 public class MarupekeCMD
 {
     static Scanner reader = new Scanner(System.in);
+    static MarupekeGrid game = null;
     
     public static void main(String[] args)
     {
         boolean running = true;
-        MarupekeGrid game = new MarupekeGrid(5);
+
+        System.out.println("MARUPEKE");
+        System.out.println("-------------------------------------------");
      
         while(running)
         {
             String command = readInput("Please enter a command: ");
-            
-            if(command.equals("quit"))
+
+            switch(command)
             {
-                running = false;
+                case "quit" ->
+                {
+                    running = false;
+                }
+                case "new" ->
+                {
+                    int size = Integer.parseInt(readInput("Game Size: "));
+                    game = MarupekeGrid.randomPuzzle(size, 5, 3, 3);
+
+                    System.out.print(game.toString());
+                }
+                case "x" ->
+                {
+                    if(game == null) break;
+
+                    int[] coords = getUserCoordinates();
+                    if(coords == null) break;
+
+                    game.setX(coords[0], coords[1], true);
+
+                    System.out.print(game.toString());
+                }
+                case "o" ->
+                {
+                    if(game == null) break;
+
+                    int[] coords = getUserCoordinates();
+                    if(coords == null) break;
+
+                    game.setO(coords[0], coords[1], true);
+
+                    System.out.print(game.toString());
+                }
+                case "unmark" ->
+                {
+                    if(game == null) break;
+
+                    int[] coords = getUserCoordinates();
+                    if(coords == null) break;
+
+                    game.unmark(coords[0], coords[1]);
+
+                    System.out.print(game.toString());
+                }
+                case "help" ->
+                {
+                    System.out.println("Commands ----------------------------------");
+                    System.out.println("'new' - Create a new game");
+                    System.out.println("'quit' - Quit the game");
+                    System.out.println("'x' - Place an 'X' mark on the grid");
+                    System.out.println("'o' - Place an 'O' mark on the grid");
+                    System.out.println("'unmark' - Un-mark a tile on the grid");
+                    System.out.println("-------------------------------------------");
+                }
             }
-            
-            else if(command.equals("new"))
-            {
-                int size = Integer.parseInt(readInput("Game Size: "));
-                game = MarupekeGrid.randomPuzzle(size, 5, 3, 3);
-                
-                System.out.print(game.toString());
-            }
-            
-            else if(command.equals("xmark"))
-            { 
-                int x = Integer.parseInt(readInput("Row Position: "));
-                int y = Integer.parseInt(readInput("Column Position: "));
-                
-                game.setX(x, y, true);
-                System.out.print(game.toString());
-            }
-            
-            else if(command.equals("omark"))
-            {
-                int x = Integer.parseInt(readInput("Row Position: "));
-                int y = Integer.parseInt(readInput("Column Position: "));
-                
-                game.setO(x, y, true);
-                System.out.print(game.toString());
-            }
-                        
-            else if(command.equals("unmark"))
-            {
-                int x = Integer.parseInt(readInput("Row Position: "));
-                int y = Integer.parseInt(readInput("Column Position: "));
-                
-                game.unmark(x, y);
-                System.out.print(game.toString());
-            }
-        }   
+        }
+
         System.out.println("Thanks for playing!");
     }
     
@@ -61,5 +83,19 @@ public class MarupekeCMD
     {
         System.out.print(prompt);
         return reader.next().toLowerCase();
+    }
+
+    private static int[] getUserCoordinates()
+    {
+        int x = Integer.parseInt(readInput("Row Position: "));
+        int y = Integer.parseInt(readInput("Column Position: "));
+
+        if(x < 0 || y < 0 || x > game.getGrid().length-1 || y > game.getGrid().length-1)
+        {
+            System.out.println("Please enter values that are within the game bounds: " + "(0-" + (game.getGrid().length - 1) + ")");
+            return null;
+        }
+
+        return new int[]{x,y};
     }
 }
